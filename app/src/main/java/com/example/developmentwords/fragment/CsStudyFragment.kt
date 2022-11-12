580f716c7d24eca8973d872f8c3d639cc70e5f6f
JangWoojun <woojun0107@naver.com>
2022-11-12 19:44:43 +0900

    FEAT : 하단 바 클릭시 다른 Fragment로 이동 추가
    히단 바 아이콘 클릭 시 다른 Fragment로 이동할 수 있게 구현

--- app/src/main/java/com/example/developmentwords/fragment/CsStudyFragment.kt
+++ app/src/main/java/com/example/developmentwords/fragment/CsStudyFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.developmentwords.R
+import com.example.developmentwords.databinding.FragmentCsStudyBinding
 
 class CsStudyFragment : Fragment() {
+    private var _binding: FragmentCsStudyBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +22,28 @@ class CsStudyFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cs_study, container, false)
+
+        _binding = FragmentCsStudyBinding.inflate(inflater, container, false)
+
+        binding.home.setOnClickListener {
+            it.findNavController().navigate(R.id.action_csStudyFragment_to_mainFragment)
+        }
+        binding.words.setOnClickListener {
+            it.findNavController().navigate(R.id.action_csStudyFragment_to_wordFragment)
+        }
+        binding.learning.setOnClickListener {
+            it.findNavController().navigate(R.id.action_csStudyFragment_to_studyFragment)
+        }
+        binding.setting.setOnClickListener {
+            it.findNavController().navigate(R.id.action_csStudyFragment_to_settingFragment)
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
--- app/src/main/java/com/example/developmentwords/fragment/EnglishStudyFragment.kt
+++ app/src/main/java/com/example/developmentwords/fragment/EnglishStudyFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.developmentwords.R
+import com.example.developmentwords.databinding.FragmentEnglishStudyBinding
 
 class EnglishStudyFragment : Fragment() {
+    private var _binding: FragmentEnglishStudyBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +22,27 @@ class EnglishStudyFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_english_study, container, false)
+        _binding = FragmentEnglishStudyBinding.inflate(inflater, container, false)
+
+        binding.home.setOnClickListener {
+            it.findNavController().navigate(R.id.action_englishStudyFragment_to_mainFragment)
+        }
+        binding.words.setOnClickListener {
+            it.findNavController().navigate(R.id.action_englishStudyFragment_to_wordFragment)
+        }
+        binding.learning.setOnClickListener {
+            it.findNavController().navigate(R.id.action_englishStudyFragment_to_studyFragment)
+        }
+        binding.setting.setOnClickListener {
+            it.findNavController().navigate(R.id.action_englishStudyFragment_to_settingFragment)
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
