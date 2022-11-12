80f6220ab832b5d2771b1e22a429ba44e2badcef
JangWoojun <woojun0107@naver.com>
2022-11-12 19:00:35 +0900

    FEAT : 다른 Fragment로 이동 구현
    히단 바 아이콘 클릭 시 다른 Fragment로 이동할 수 있게 구현

--- app/src/main/java/com/example/developmentwords/fragment/CsWordFragment.kt
+++ app/src/main/java/com/example/developmentwords/fragment/CsWordFragment.kt
@@ -5,52 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.developmentwords.R
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.developmentwords.databinding.FragmentCsWordBinding
+import com.example.developmentwords.databinding.FragmentMainBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CsWordFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class CsWordFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentCsWordBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCsWordBinding.inflate(inflater, container, false)
+
+        binding.home.setOnClickListener {
+            it.findNavController().navigate(R.id.action_csWordFragment_to_mainFragment)
         }
+        binding.words.setOnClickListener {
+            it.findNavController().navigate(R.id.action_csWordFragment_to_wordFragment)
+        }
+        binding.learning.setOnClickListener {
+            it.findNavController().navigate(R.id.action_csWordFragment_to_studyFragment)
+        }
+        binding.setting.setOnClickListener {
+            it.findNavController().navigate(R.id.action_csWordFragment_to_settingFragment)
+        }
+
+        return binding.root
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cs_word, container, false)
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CsWordFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                CsWordFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
-    }
 }
--- app/src/main/java/com/example/developmentwords/fragment/EnglishWordFragment.kt
+++ app/src/main/java/com/example/developmentwords/fragment/EnglishWordFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.developmentwords.R
 import com.example.developmentwords.databinding.FragmentEnglishWordBinding
 
 class EnglishWordFragment : Fragment() {
@@ -21,6 +23,19 @@ class EnglishWordFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentEnglishWordBinding.inflate(inflater, container, false)
+
+        binding.home.setOnClickListener {
+            it.findNavController().navigate(R.id.action_englishWordFragment_to_mainFragment)
+        }
+        binding.words.setOnClickListener {
+            it.findNavController().navigate(R.id.action_englishWordFragment_to_wordFragment)
+        }
+        binding.learning.setOnClickListener {
+            it.findNavController().navigate(R.id.action_englishWordFragment_to_studyFragment)
+        }
+        binding.setting.setOnClickListener {
+            it.findNavController().navigate(R.id.action_englishWordFragment_to_settingFragment)
+        }
 
         return binding.root
     }
