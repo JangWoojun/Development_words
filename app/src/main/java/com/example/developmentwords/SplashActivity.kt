498f6d4325bb9d7d6994c876b08e2c3dc06e9e47
JangWoojun <woojun0107@naver.com>
2022-11-16 01:41:07 +0900

    FEAT : Splash 화면에서 로그인 기능 추가
    기존 유저인지 판단해서 기존 유저면 그냥 MainActivity로 이동,신규 유저면 비회원 로그인 시켜서 MainActivity로 이동

--- app/src/main/java/com/example/developmentwords/SplashActivity.kt
+++ app/src/main/java/com/example/developmentwords/SplashActivity.kt
@@ -4,15 +4,40 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        auth = Firebase.auth
+
+        if(auth.currentUser?.uid == null){
+            Handler().postDelayed({
+                auth.signInAnonymously()
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "환영합니다", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this,MainActivity::class.java))
+                            finish()
+                        } else {
+                            Toast.makeText(this, "오류", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            },2000)
+        }
+        else {
+            Handler().postDelayed({
+                Toast.makeText(this,"다시 돌아오신 걸 환영합니다",Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this,MainActivity::class.java))
+                finish()
+            },2000)
+        }
 
     }
 }
