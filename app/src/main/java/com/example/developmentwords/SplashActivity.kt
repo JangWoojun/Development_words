f2dd68597247aca5396058f27e01cb921bd3229c
JangWoojun <woojun0107@naver.com>
2022-11-16 09:05:37 +0900

    FEAT : 신규유저 정보 데이터베이스 등록
    Realtime Database에 신규유저 기본 정보 등록

--- app/src/main/java/com/example/developmentwords/SplashActivity.kt
+++ app/src/main/java/com/example/developmentwords/SplashActivity.kt
@@ -7,21 +7,29 @@ import android.os.Handler
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         auth = Firebase.auth
+        database = Firebase.database.reference
 
         if(auth.currentUser?.uid == null){
             Handler().postDelayed({
                 auth.signInAnonymously()
                     .addOnCompleteListener(this) { task ->
                         if (task.isSuccessful) {
+                            database.child("users").child(auth.currentUser?.uid.toString())
+                                .child("csLevel").setValue(1)
+                            database.child("users").child(auth.currentUser?.uid.toString())
+                                .child("englishLevel").setValue(1)
                             Toast.makeText(this, "환영합니다", Toast.LENGTH_SHORT).show()
                             startActivity(Intent(this,MainActivity::class.java))
                             finish()
@@ -33,6 +41,7 @@ class SplashActivity : AppCompatActivity() {
         }
         else {
             Handler().postDelayed({
+
                 Toast.makeText(this,"다시 돌아오신 걸 환영합니다",Toast.LENGTH_SHORT).show()
                 startActivity(Intent(this,MainActivity::class.java))
                 finish()
