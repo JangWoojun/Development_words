ddb16c78687f03c3eb043ac140d945394d8f3ba2
JangWoojun <woojun0107@naver.com>
2022-11-16 10:22:14 +0900

    FEAT : 실시간으로 내 스테이터스 알려주는 기능 추가
    Realtime Database를 연동하여 실시간으로 내 스테이터스를 알려주는 기능 추가

--- app/src/main/java/com/example/developmentwords/fragment/MainFragment.kt
+++ app/src/main/java/com/example/developmentwords/fragment/MainFragment.kt
@@ -1,6 +1,8 @@
 package com.example.developmentwords.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,24 +10,40 @@ import android.view.ViewGroup
 import androidx.navigation.findNavController
 import com.example.developmentwords.R
 import com.example.developmentwords.databinding.FragmentMainBinding
-import com.example.developmentwords.databinding.FragmentSettingBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainFragment : Fragment() {
-
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    val TAG = "MainFragment"
 
-    }
-
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
         _binding = FragmentMainBinding.inflate(inflater, container, false)
+
+        auth = Firebase.auth
+        database = Firebase.database.reference
+
+        database.child("users").child(auth.currentUser?.uid.toString())
+            .child("csLevel").get().addOnSuccessListener {
+                val csLevel = it.value
+                database.child("users").child(auth.currentUser?.uid.toString())
+                    .child("englishLevel").get().addOnSuccessListener {
+                        binding.statText.text = "유저님의 스테이터스는\nCS Level$csLevel / 영어 Level${it.value} 입니다"
+                    }
+            }
+
 
         binding.words.setOnClickListener{
             it.findNavController().navigate(R.id.action_mainFragment_to_wordFragment)
