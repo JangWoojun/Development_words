ba4b483aaff0000d4645d2c6f255e21c973a2bfb
JangWoojun <woojun0107@naver.com>
2022-11-18 14:39:21 +0900

    FEAT : RecyclerView item 클릭 처리 추가
    RecyclerView item 클릭 시 숨겨져있는 설명이 나오는 기능 추가

--- app/src/main/java/com/example/developmentwords/recyclerview/EnglishWordAdapter.kt
+++ app/src/main/java/com/example/developmentwords/recyclerview/EnglishWordAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.developmentwords.recyclerview
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.developmentwords.databinding.ListItemBinding
@@ -9,12 +10,24 @@ class EnglishWordAdapter (private val englishwords : List<Word>) :
     RecyclerView.Adapter<EnglishWordAdapter.EnglishWordViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EnglishWordViewHolder {
-        return EnglishWordViewHolder(
-            ListItemBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent, false
-            )
+        val binding = ListItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent, false
         )
+        return EnglishWordViewHolder(binding).also {
+
+            binding.wordBox.setOnClickListener {
+                if (binding.listItemText2.visibility == View.VISIBLE){
+                    binding.listItemText2.visibility = View.GONE
+                    binding.listItemText3.visibility = View.VISIBLE
+                }
+                else {
+                    binding.listItemText2.visibility = View.VISIBLE
+                    binding.listItemText3.visibility = View.GONE
+                }
+
+            }
+        }
     }
 
     override fun onBindViewHolder(holder: EnglishWordViewHolder, position: Int) {
