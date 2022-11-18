733e9141b685c1358a18963165fe79ebfaa418e3
JangWoojun <woojun0107@naver.com>
2022-11-18 10:53:53 +0900

    FEAT : CsWordAdapter 연결
    RecyclerView와 CsWordAdapter 연결 및 임시 아이템 생성

--- app/src/main/java/com/example/developmentwords/fragment/CsWordFragment.kt
+++ app/src/main/java/com/example/developmentwords/fragment/CsWordFragment.kt
@@ -6,14 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.developmentwords.R
 import com.example.developmentwords.databinding.FragmentCsWordBinding
-import com.example.developmentwords.databinding.FragmentMainBinding
+import com.example.developmentwords.recyclerview.CsWordAdapter
+import com.example.developmentwords.recyclerview.Word
 
 
 class CsWordFragment : Fragment() {
     private var _binding: FragmentCsWordBinding? = null
     private val binding get() = _binding!!
+    private val csWords = listOf(
+        Word("네카라쿠베1"),
+        Word("네카라쿠베2"),
+        Word("네카라쿠베3"),
+        Word("네카라쿠베4"),
+        Word("네카라쿠베5"),
+        Word("네카라쿠베6"),
+        Word("네카라쿠베7"),
+        Word("네카라쿠베8"),
+        Word("네카라쿠베9"),
+        )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,12 +52,19 @@ class CsWordFragment : Fragment() {
             it.findNavController().navigate(R.id.action_csWordFragment_to_settingFragment)
         }
 
+        initializeViews()
+
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun initializeViews() {
+        binding.list.layoutManager = LinearLayoutManager(context)
+        binding.list.adapter = CsWordAdapter(csWords)
     }
 
 }
