04d0e915ca74ee363eae76386083fade2ca1b0ee
JangWoojun <woojun0107@naver.com>
2022-11-19 05:45:59 +0900

    FEAT : Realtime Database 연결
    Firebase Realtime Database와 연결하여 현재 단어장에 레벨을 가져옴

--- app/src/main/java/com/example/developmentwords/fragment/CsWordFragment.kt
+++ app/src/main/java/com/example/developmentwords/fragment/CsWordFragment.kt
@@ -1,6 +1,7 @@
 package com.example.developmentwords.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,11 +12,20 @@ import com.example.developmentwords.R
 import com.example.developmentwords.databinding.FragmentCsWordBinding
 import com.example.developmentwords.recyclerview.CsWordAdapter
 import com.example.developmentwords.recyclerview.Word
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 
 class CsWordFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
     private var _binding: FragmentCsWordBinding? = null
     private val binding get() = _binding!!
+    val TAG = "CsWordFragment"
     private val csWords = listOf(
         Word("네카라쿠베1","이것은 네카라쿠베1 설명입니다"),
         Word("네카라쿠베2","이것은 네카라쿠베2 설명입니다"),
@@ -51,6 +61,23 @@ class CsWordFragment : Fragment() {
         binding.setting.setOnClickListener {
             it.findNavController().navigate(R.id.action_csWordFragment_to_settingFragment)
         }
+
+        auth = Firebase.auth
+        val database = Firebase.database
+        val myRef = database.getReference("users").child(auth.currentUser?.uid.toString()).child("csLevel")
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.value
+                binding.csWordLv.text = getString(R.string.cs_word_lv,value)
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
 
         initializeViews()
 
