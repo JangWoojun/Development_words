2bc0fd8d175b6bb4232e054e47d3bc2e4d5a15df
JangWoojun <woojun0107@naver.com>
2022-11-21 13:25:47 +0900

    FEAT : 유저 레벨 받아오는 기능 추가
    데이터베이스에서 유저 레벨 받아오는 함수 추가

--- app/src/main/java/com/example/developmentwords/fragment/EnglishStudyFragment.kt
+++ app/src/main/java/com/example/developmentwords/fragment/EnglishStudyFragment.kt
@@ -1,6 +1,8 @@
 package com.example.developmentwords.fragment
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,10 +10,15 @@ import android.view.ViewGroup
 import androidx.navigation.findNavController
 import com.example.developmentwords.R
 import com.example.developmentwords.databinding.FragmentEnglishStudyBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class EnglishStudyFragment : Fragment() {
     private var _binding: FragmentEnglishStudyBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,21 +30,39 @@ class EnglishStudyFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentEnglishStudyBinding.inflate(inflater, container, false)
+        auth = Firebase.auth
 
-        binding.home.setOnClickListener {
-            it.findNavController().navigate(R.id.action_englishStudyFragment_to_mainFragment)
-        }
-        binding.words.setOnClickListener {
-            it.findNavController().navigate(R.id.action_englishStudyFragment_to_wordFragment)
-        }
-        binding.learning.setOnClickListener {
-            it.findNavController().navigate(R.id.action_englishStudyFragment_to_studyFragment)
-        }
-        binding.setting.setOnClickListener {
-            it.findNavController().navigate(R.id.action_englishStudyFragment_to_settingFragment)
-        }
+        var i = 1
+
+        get(i)
+
+        var chk = 0
+
+
 
         return binding.root
+    }
+
+    fun get(i : Int){
+        val database = Firebase.database
+        val level = database.getReference("users").child(auth.currentUser?.uid.toString()).child("englishLevel")
+
+        Log.d("여기",level.toString())
+
+        level.get().addOnSuccessListener{
+            val value = it.value
+            binding.csWordLv.text = "영어 Lv.$value"
+            val word = database.getReference("englishWord").child("lv$value").child("$i").child("word")
+            val mean = database.getReference("englishWord").child("lv$value").child("$i").child("mean")
+
+            word.get().addOnSuccessListener {
+                binding.word.text = it.value.toString()
+            }
+            mean.get().addOnSuccessListener {
+                binding.mean.text = it.value.toString()
+            }
+
+        }
     }
 
     override fun onDestroyView() {
