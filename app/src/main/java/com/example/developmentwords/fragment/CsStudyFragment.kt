eab85b3ed0b9ba00bf6f3d9220950dfbe3e4bbc2
JangWoojun <woojun0107@naver.com>
2022-11-21 14:28:59 +0900

    FEAT : 유저 레벨 받아오기 추가
    데이터베이스에서 유저 데이터 받아오는 함수 추가

--- app/src/main/java/com/example/developmentwords/fragment/CsStudyFragment.kt
+++ app/src/main/java/com/example/developmentwords/fragment/CsStudyFragment.kt
@@ -1,18 +1,26 @@
 package com.example.developmentwords.fragment
 
+import android.animation.ObjectAnimator
+import android.content.Context
+import android.content.Context.VIBRATOR_SERVICE
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Vibrator
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
+import androidx.fragment.app.Fragment
 import com.example.developmentwords.R
 import com.example.developmentwords.databinding.FragmentCsStudyBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
 
 class CsStudyFragment : Fragment() {
     private var _binding: FragmentCsStudyBinding? = null
     private val binding get() = _binding!!
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,21 +32,41 @@ class CsStudyFragment : Fragment() {
     ): View? {
 
         _binding = FragmentCsStudyBinding.inflate(inflater, container, false)
+        auth = Firebase.auth
 
-        binding.home.setOnClickListener {
-            it.findNavController().navigate(R.id.action_csStudyFragment_to_mainFragment)
-        }
-        binding.words.setOnClickListener {
-            it.findNavController().navigate(R.id.action_csStudyFragment_to_wordFragment)
-        }
-        binding.learning.setOnClickListener {
-            it.findNavController().navigate(R.id.action_csStudyFragment_to_studyFragment)
-        }
-        binding.setting.setOnClickListener {
-            it.findNavController().navigate(R.id.action_csStudyFragment_to_settingFragment)
-        }
+        var i = 1
+
+        get(i)
+
+        var chk = 0
+
+
 
         return binding.root
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    fun get(i : Int){
+        val database = Firebase.database
+        val level = database.getReference("users").child(auth.currentUser?.uid.toString()).child("csLevel")
+
+        level.get().addOnSuccessListener{
+            val value = it.value
+            binding.csWordLv.text = "CS Lv.$value"
+            val word = database.getReference("csWord").child("lv$value").child("$i").child("word")
+            val mean = database.getReference("csWord").child("lv$value").child("$i").child("mean")
+
+            word.get().addOnSuccessListener {
+                binding.word.text = it.value.toString()
+            }
+            mean.get().addOnSuccessListener {
+                binding.mean.text = it.value.toString()
+            }
+
+        }
     }
 
     override fun onDestroyView() {
