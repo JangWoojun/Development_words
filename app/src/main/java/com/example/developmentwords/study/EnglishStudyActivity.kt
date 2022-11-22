22b99ad37308deb84d937b8aeae2bc304af3ca9d
JangWoojun <woojun0107@naver.com>
2022-11-22 13:18:28 +0900

    FEAT : CardStack Adapter 연결
    CardStack Adapter와 연결

--- app/src/main/java/com/example/developmentwords/study/EnglishStudyActivity.kt
+++ app/src/main/java/com/example/developmentwords/study/EnglishStudyActivity.kt
@@ -16,11 +16,66 @@ import com.yuyakaido.android.cardstackview.CardStackListener
 import com.yuyakaido.android.cardstackview.Direction
 
 class EnglishStudyActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEnglishStudyBinding
+    lateinit var cardStackAdapter: CardStackAdapter
+    lateinit var manager: CardStackLayoutManager
+    private val list = mutableListOf<Voca>()
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_english_study)
+        binding = ActivityEnglishStudyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
 
+        manager = CardStackLayoutManager(baseContext,object : CardStackListener {
+            override fun onCardDragging(direction: Direction?, ratio: Float) {
+            }
+
+            override fun onCardSwiped(direction: Direction?) {
+            }
+
+            override fun onCardRewound() {
+            }
+
+            override fun onCardCanceled() {
+            }
+
+            override fun onCardAppeared(view: View?, position: Int) {
+            }
+
+            override fun onCardDisappeared(view: View?, position: Int) {
+            }
+
+        })
+
+        cardStackAdapter = CardStackAdapter(list)
+        binding.cardStackView.layoutManager = manager
+        binding.cardStackView.adapter = cardStackAdapter
+
+        get()
     }
+    fun get(){
+        val database = Firebase.database
+        val level = database.getReference("users").child(auth.currentUser?.uid.toString()).child("englishLevel")
 
+        level.get().addOnSuccessListener{
+
+            val value = it.value
+            binding.csWordLv.text = "영어 Lv.$value"
+            val voca = database.getReference("englishWord").child("lv$value")
+            Log.d("여기",voca.toString())
+
+            voca.get().addOnSuccessListener {
+                val value : ArrayList<Any> = it.value as ArrayList<Any>
+                for (i in 1..15){
+                    val valueMap: HashMap<String,String> = value[i] as HashMap<String, String>
+
+                    list.add(Voca(valueMap["word"]!!.toString(),valueMap["mean"]!!.toString()))
+                }
+                binding.cardStackView.adapter?.notifyDataSetChanged()
+            }
+        }
+    }
 }
