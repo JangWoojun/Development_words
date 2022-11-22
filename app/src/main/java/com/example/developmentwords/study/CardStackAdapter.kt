2498b149f7018dd9768f561da3e01a6d4d37788f
JangWoojun <woojun0107@naver.com>
2022-11-22 13:12:10 +0900

    FEAT : Adapter 추가
    CardStackAdapter 추가

---
+++ app/src/main/java/com/example/developmentwords/study/CardStackAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.developmentwords.study
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.developmentwords.databinding.CardItemBinding
+import com.example.developmentwords.recyclerview.Voca
+
+class CardStackAdapter{
+
+}
