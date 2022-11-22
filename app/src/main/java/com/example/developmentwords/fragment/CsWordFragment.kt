5423eaca77258cd7c2a1ad7e64982fc5ae954d24
JangWoojun <woojun0107@naver.com>
2022-11-22 13:02:50 +0900

    REFACTOR : 이름 리팩토링
    통합 Adapter 및 새로운 Data class로 교체

--- app/src/main/java/com/example/developmentwords/fragment/CsWordFragment.kt
+++ app/src/main/java/com/example/developmentwords/fragment/CsWordFragment.kt
@@ -1,7 +1,6 @@
 package com.example.developmentwords.fragment
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,13 +9,10 @@ import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.developmentwords.R
 import com.example.developmentwords.databinding.FragmentCsWordBinding
-import com.example.developmentwords.recyclerview.CsWordAdapter
-import com.example.developmentwords.recyclerview.Word
+import com.example.developmentwords.recyclerview.Voca
+import com.example.developmentwords.recyclerview.WordAdapter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
@@ -26,7 +22,7 @@ class CsWordFragment : Fragment() {
     private var _binding: FragmentCsWordBinding? = null
     private val binding get() = _binding!!
     val TAG = "CsWordFragment"
-    private val csWords = ArrayList<Word>()
+    private val csWords = ArrayList<Voca>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,31 +49,35 @@ class CsWordFragment : Fragment() {
             it.findNavController().navigate(R.id.action_csWordFragment_to_settingFragment)
         }
 
+
+        
+        firebase()
+        initializeViews()
+
+        return binding.root
+    }
+
+    private fun firebase(){
         auth = Firebase.auth
         val database = Firebase.database
         val levelRef = database.getReference("users").child(auth.currentUser?.uid.toString()).child("csLevel")
 
         levelRef.get().addOnSuccessListener {
-                val value = it.value
-                binding.csWordLv.text = getString(R.string.cs_word_lv,value)
-                val wordRef = database.getReference("csWord").child("lv"+value.toString())
+            val value = it.value
+            binding.csWordLv.text = getString(R.string.cs_word_lv,value)
+            val wordRef = database.getReference("csWord").child("lv"+value.toString())
 
-                wordRef.get().addOnSuccessListener { it ->
-                    val value : ArrayList<Any> = it.value as ArrayList<Any>
-                        for (i in 1..15){
-                            val valueMap: HashMap<String,String> = value[i] as HashMap<String, String>
+            wordRef.get().addOnSuccessListener { it ->
+                val value : ArrayList<Any> = it.value as ArrayList<Any>
+                for (i in 1..15){
+                    val valueMap: HashMap<String,String> = value[i] as HashMap<String, String>
 
-                            csWords.add(Word(valueMap["word"]!!.toString(),valueMap["mean"]!!.toString()))
-                        }
-                        binding.list.adapter?.notifyDataSetChanged()
-                    }
-
+                    csWords.add(Voca(valueMap["word"]!!.toString(),valueMap["mean"]!!.toString()))
+                }
+                binding.list.adapter?.notifyDataSetChanged()
             }
-        
 
-        initializeViews()
-
-        return binding.root
+        }
     }
 
     override fun onDestroyView() {
@@ -87,7 +87,7 @@ class CsWordFragment : Fragment() {
 
     private fun initializeViews() {
         binding.list.layoutManager = LinearLayoutManager(context)
-        binding.list.adapter = CsWordAdapter(csWords)
+        binding.list.adapter = WordAdapter(csWords)
     }
 
 }
--- app/src/main/java/com/example/developmentwords/fragment/EnglishWordFragment.kt
+++ app/src/main/java/com/example/developmentwords/fragment/EnglishWordFragment.kt
@@ -1,7 +1,6 @@
 package com.example.developmentwords.fragment
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,13 +9,10 @@ import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.developmentwords.R
 import com.example.developmentwords.databinding.FragmentEnglishWordBinding
-import com.example.developmentwords.recyclerview.EnglishWordAdapter
-import com.example.developmentwords.recyclerview.Word
+import com.example.developmentwords.recyclerview.Voca
+import com.example.developmentwords.recyclerview.WordAdapter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
@@ -25,7 +21,7 @@ class EnglishWordFragment : Fragment() {
     private val binding get() = _binding!!
     val TAG = "EnglishWordFragment"
     private lateinit var auth: FirebaseAuth
-    private val englishWords = ArrayList<Word>()
+    private val englishWords = ArrayList<Voca>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -66,7 +62,7 @@ class EnglishWordFragment : Fragment() {
                         for (i in 1..15){
                             val valueMap: HashMap<String,String> = value[i] as HashMap<String, String>
 
-                            englishWords.add(Word(valueMap["word"]!!.toString(),valueMap["mean"]!!.toString()))
+                            englishWords.add(Voca(valueMap["word"]!!.toString(),valueMap["mean"]!!.toString()))
                         }
                         binding.list.adapter?.notifyDataSetChanged()
                     }
@@ -84,6 +80,6 @@ class EnglishWordFragment : Fragment() {
     }
     private fun initializeViews() {
         binding.list.layoutManager = LinearLayoutManager(context)
-        binding.list.adapter = EnglishWordAdapter(englishWords)
+        binding.list.adapter = WordAdapter(englishWords)
     }
 }
