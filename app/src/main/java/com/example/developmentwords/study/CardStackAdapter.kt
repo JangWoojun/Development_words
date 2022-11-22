7ca38f9262e531470539e940618894e0adb1669b
JangWoojun <woojun0107@naver.com>
2022-11-22 13:17:43 +0900

    FEAT : CardStackView Adapter 제작
    CardStackView에 사용할 Adapter 제작

--- app/src/main/java/com/example/developmentwords/study/CardStackAdapter.kt
+++ app/src/main/java/com/example/developmentwords/study/CardStackAdapter.kt
@@ -6,6 +6,30 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.developmentwords.databinding.CardItemBinding
 import com.example.developmentwords.recyclerview.Voca
 
-class CardStackAdapter{
+class CardStackAdapter(private val items:List<Voca>) :
+    RecyclerView.Adapter<CardStackAdapter.CardViewHolder>() {
+
+    override fun onCreateViewHolder(parent:ViewGroup,viewType:Int) : CardStackAdapter.CardViewHolder{
+        val binding = CardItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,false
+        )
+        return CardViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CardStackAdapter.CardViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class CardViewHolder(private val binding: CardItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(data:Voca){
+            binding.wordText.text = data.word
+            binding.meanText.text = data.mean
+        }
+    }
 
 }
