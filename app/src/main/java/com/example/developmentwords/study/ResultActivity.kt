2a47d89e87bde901b9bc5c012d4461d8f80c7eb0
JangWoojun <woojun0107@naver.com>
2022-11-23 22:23:37 +0900

    FEAT : 학습 결과 보여주는 기능
    학습하기 액티비티에서 학습한 결과 보여주는 기능 추가

--- app/src/main/java/com/example/developmentwords/study/ResultActivity.kt
+++ app/src/main/java/com/example/developmentwords/study/ResultActivity.kt
@@ -1,13 +1,35 @@
 package com.example.developmentwords.study
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import com.example.developmentwords.MainActivity
 import com.example.developmentwords.R
+import com.example.developmentwords.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+
+        val o = intent.getStringExtra("o")
+        val x = intent.getStringExtra("x")
+        val p = (((o?.toFloat())?.div(15))?.times(100))?.toInt()
+
+        val tv8 = findViewById<TextView>(R.id.textView8)
+        val tv9 = findViewById<TextView>(R.id.textView9)
+        val tv10 = findViewById<TextView>(R.id.textView10)
+
+        tv8.text = getString(R.string.result_text4,o)
+        tv9.text = getString(R.string.result_text5,x)
+        tv10.text = "$p%"
+
+
 
     }
 }
