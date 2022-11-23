c3809e5e04a0226d519098813eac346c23b6a57c
JangWoojun <woojun0107@naver.com>
2022-11-23 22:28:59 +0900

    FEAT : 홈화면으로 이동
    결과 확인 이후 홈화면으로 이동시키는 기능 추가

--- app/src/main/java/com/example/developmentwords/study/ResultActivity.kt
+++ app/src/main/java/com/example/developmentwords/study/ResultActivity.kt
@@ -3,7 +3,6 @@ package com.example.developmentwords.study
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import com.example.developmentwords.MainActivity
@@ -24,12 +23,17 @@ class ResultActivity : AppCompatActivity() {
         val tv8 = findViewById<TextView>(R.id.textView8)
         val tv9 = findViewById<TextView>(R.id.textView9)
         val tv10 = findViewById<TextView>(R.id.textView10)
+        val button1 = findViewById<Button>(R.id.button1)
 
         tv8.text = getString(R.string.result_text4,o)
         tv9.text = getString(R.string.result_text5,x)
         tv10.text = "$p%"
 
-
+        button1.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
     }
 }
