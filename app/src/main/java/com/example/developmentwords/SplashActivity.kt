086a4b0256ae2d5ca8f28c1a85931ac337053ac1
JangWoojun <woojun0107@naver.com>
2022-11-27 02:22:16 +0900

    FEAT : Toast 메시지 변경
    Toast 메시지 라이브러리를 사용하여 Toast 메시지 꾸미기

--- app/src/main/java/com/example/developmentwords/SplashActivity.kt
+++ app/src/main/java/com/example/developmentwords/SplashActivity.kt
@@ -1,15 +1,18 @@
 package com.example.developmentwords
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import es.dmoral.toasty.Toasty
+import java.text.SimpleDateFormat
+import java.util.*
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
@@ -17,6 +20,14 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val formatYYYYMMdd = "yyyyMMdd"
+
+        val currentTime: Date = Calendar.getInstance().time
+
+        val format = SimpleDateFormat(formatYYYYMMdd, Locale.getDefault())
+        val current: String = format.format(currentTime)
+
 
         auth = Firebase.auth
         database = Firebase.database.reference
@@ -30,19 +41,18 @@ class SplashActivity : AppCompatActivity() {
                                 .child("csLevel").setValue(1)
                             database.child("users").child(auth.currentUser?.uid.toString())
                                 .child("englishLevel").setValue(1)
-                            Toast.makeText(this, "환영합니다", Toast.LENGTH_SHORT).show()
+                            Toasty.normal(this, "환영합니다", Toast.LENGTH_SHORT,getDrawable(R.drawable.home)).show()
                             startActivity(Intent(this,MainActivity::class.java))
                             finish()
                         } else {
-                            Toast.makeText(this, "오류", Toast.LENGTH_SHORT).show()
+                            Toasty.warning(this, "오류", Toast.LENGTH_SHORT, true).show()
                         }
                     }
             },2000)
         }
         else {
             Handler().postDelayed({
-
-                Toast.makeText(this,"다시 돌아오신 걸 환영합니다",Toast.LENGTH_SHORT).show()
+                Toasty.normal(this,"다시 돌아오신 걸 환영합니다",getDrawable(R.drawable.home)).show()
                 startActivity(Intent(this,MainActivity::class.java))
                 finish()
             },2000)
