57f4edfdc45a30ab1480e878834c8de457662807
JangWoojun <woojun0107@naver.com>
2022-11-26 16:28:25 +0900

    FEAT : 퀴즈 로직 구현
    간단한 퀴즈 로직 구현

--- app/src/main/java/com/example/developmentwords/quiz/QuizActivity.kt
+++ app/src/main/java/com/example/developmentwords/quiz/QuizActivity.kt
@@ -1,10 +1,19 @@
 package com.example.developmentwords.quiz
 
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.os.postDelayed
+import com.example.developmentwords.MainActivity
 import com.example.developmentwords.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -13,6 +22,7 @@ import com.google.firebase.ktx.Firebase
 
 class QuizActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private val handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,53 +30,258 @@ class QuizActivity : AppCompatActivity() {
         val type = intent.getStringExtra("type")
         auth = Firebase.auth
 
-        val word:TextView = findViewById(R.id.word)
+        Log.d("확인",type.toString())
+
+        if (type == "english"){
+            get("english",1)
+        }
+        else {
+            get("cs",1)
+        }
+
+    }
+    fun get(type : String,i: Int){
+        if (i==16){
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        else{
+            val database = Firebase.database
+            val level = database.getReference("users").child(auth.currentUser?.uid.toString()).child("${type}Level")
+            val j = i+1
+            level.get().addOnSuccessListener{
+
+                val value1 = it.value
+                val quiz = database.getReference("${type}Quiz").child("lv$value1")
+
+                quiz.get().addOnSuccessListener {
+                    val value2 : ArrayList<Any> = it.value as ArrayList<Any>
+                    val valueMap: HashMap<String,String> = value2[i] as HashMap<String, String>
+
+                    val word:TextView = findViewById(R.id.word)
+                    val tvOptionOne:Button = findViewById(R.id.tv_option_one)
+                    val tvOptionTwo:Button = findViewById(R.id.tv_option_two)
+                    val tvOptionThree:Button = findViewById(R.id.tv_option_three)
+                    val tvOptionFour:Button = findViewById(R.id.tv_option_four)
+                    val item1 : ImageView = findViewById(R.id.cardItem1)
+                    val item2 : ImageView = findViewById(R.id.cardItem2)
+
+                    word.text = valueMap["word"]
+                    tvOptionOne.text = valueMap["option1"]
+                    tvOptionTwo.text = valueMap["option2"]
+                    tvOptionThree.text = valueMap["option3"]
+                    tvOptionFour.text = valueMap["option4"]
+
+                    tvOptionOne.isClickable = true
+                    tvOptionTwo.isClickable = true
+                    tvOptionThree.isClickable = true
+                    tvOptionFour.isClickable = true
+
+                    val chk = valueMap["chk"]?.toInt()
+
+
+                    tvOptionOne.setOnClickListener {
+
+                        val click = 1
+                        tvOptionTwo.isClickable = false
+                        tvOptionThree.isClickable = false
+                        tvOptionFour.isClickable = false
+
+                        if (chk == click){
+                            it.setBackgroundResource(R.drawable.radius_green)
+                            tvOptionOne.setTextColor(Color.parseColor("#ffffff"))
+                            tvOptionOne.setTypeface( null, Typeface.BOLD )
+                            item1.visibility = View.VISIBLE
+
+                            handler.postDelayed({
+                                get(type,j)
+                            },100)
+
+                            handler.postDelayed({
+                                item1.visibility = View.GONE
+
+                                clear()
+
+                            }, 500)
+
+                        }
+                        else {
+                            it.setBackgroundResource(R.drawable.radius_red)
+                            tvOptionOne.setTextColor(Color.parseColor("#ffffff"))
+                            tvOptionOne.setTypeface( null, Typeface.BOLD )
+                            item2.visibility = View.VISIBLE
+
+                            handler.postDelayed({
+                                get(type,j)
+                            },100)
+
+                            handler.postDelayed({
+                                item2.visibility = View.GONE
+
+                                clear()
+
+                            }, 500)
+
+                        }
+                    }
+                    tvOptionTwo.setOnClickListener {
+                        val click = 2
+
+                        tvOptionOne.isClickable = false
+                        tvOptionThree.isClickable = false
+                        tvOptionFour.isClickable = false
+
+                        if (chk == click){
+                            it.setBackgroundResource(R.drawable.radius_green)
+                            tvOptionTwo.setTextColor(Color.parseColor("#ffffff"))
+                            tvOptionTwo.setTypeface( null, Typeface.BOLD )
+                            item1.visibility = View.VISIBLE
+
+                            handler.postDelayed({
+                                get(type,j)
+                            },100)
+
+                            handler.postDelayed({
+                                item1.visibility = View.GONE
+
+                                clear()
+
+                            }, 500)
+
+                        }
+                        else {
+                            it.setBackgroundResource(R.drawable.radius_red)
+                            tvOptionTwo.setTextColor(Color.parseColor("#ffffff"))
+                            tvOptionTwo.setTypeface( null, Typeface.BOLD )
+                            item2.visibility = View.VISIBLE
+
+                            handler.postDelayed({
+                                get(type,j)
+                            },100)
+
+                            handler.postDelayed({
+                                item2.visibility = View.GONE
+
+                                clear()
+
+                            }, 500)
+
+                        }
+                    }
+                    tvOptionThree.setOnClickListener {
+                        val click = 3
+
+                        tvOptionTwo.isClickable = false
+                        tvOptionOne.isClickable = false
+                        tvOptionFour.isClickable = false
+
+                        if (chk == click){
+                            it.setBackgroundResource(R.drawable.radius_green)
+                            tvOptionThree.setTextColor(Color.parseColor("#ffffff"))
+                            tvOptionThree.setTypeface( null, Typeface.BOLD )
+                            item1.visibility = View.VISIBLE
+
+                            handler.postDelayed({
+                                get(type,j)
+                            },100)
+
+                            handler.postDelayed({
+                                item1.visibility = View.GONE
+
+                                clear()
+
+                            }, 500)
+
+                        }
+                        else {
+                            it.setBackgroundResource(R.drawable.radius_red)
+                            tvOptionThree.setTextColor(Color.parseColor("#ffffff"))
+                            tvOptionThree.setTypeface( null, Typeface.BOLD )
+                            item2.visibility = View.VISIBLE
+
+                            handler.postDelayed({
+                                get(type,j)
+                            },100)
+
+                            handler.postDelayed({
+                                item2.visibility = View.GONE
+
+                                clear()
+
+                            }, 500)
+
+                        }
+                    }
+                    tvOptionFour.setOnClickListener {
+                        val click = 4
+
+                        tvOptionTwo.isClickable = false
+                        tvOptionThree.isClickable = false
+                        tvOptionOne.isClickable = false
+
+                        if (chk == click){
+                            it.setBackgroundResource(R.drawable.radius_green)
+                            tvOptionFour.setTextColor(Color.parseColor("#ffffff"))
+                            tvOptionFour.setTypeface( null, Typeface.BOLD )
+                            item1.visibility = View.VISIBLE
+
+                            handler.postDelayed({
+                                get(type,j)
+                            },100)
+
+                            handler.postDelayed({
+                                item1.visibility = View.GONE
+
+                                clear()
+
+                            }, 500)
+
+                        }
+                        else {
+                            it.setBackgroundResource(R.drawable.radius_red)
+                            tvOptionFour.setTextColor(Color.parseColor("#ffffff"))
+                            tvOptionFour.setTypeface( null, Typeface.BOLD )
+                            item2.visibility = View.VISIBLE
+
+                            handler.postDelayed({
+                                get(type,j)
+                            },100)
+
+                            handler.postDelayed({
+                                item2.visibility = View.GONE
+
+                                clear()
+
+                            }, 500)
+
+                        }
+                    }
+
+
+                }
+
+            }
+        }
+
+    }
+    private fun clear(){
         val tvOptionOne:Button = findViewById(R.id.tv_option_one)
         val tvOptionTwo:Button = findViewById(R.id.tv_option_two)
         val tvOptionThree:Button = findViewById(R.id.tv_option_three)
         val tvOptionFour:Button = findViewById(R.id.tv_option_four)
 
-        Log.d("확인",type.toString())
+        tvOptionOne.setBackgroundResource(R.drawable.radius_white)
+        tvOptionOne.setTextColor(Color.parseColor("#7A8089"))
 
-        var i = 1
+        tvOptionTwo.setBackgroundResource(R.drawable.radius_white)
+        tvOptionTwo.setTextColor(Color.parseColor("#7A8089"))
 
-        if (type == "english"){
-            get("english",i)
-        }
-        else {
-            get("cs",i)
-        }
+        tvOptionThree.setBackgroundResource(R.drawable.radius_white)
+        tvOptionThree.setTextColor(Color.parseColor("#7A8089"))
 
-    }
-    fun get(type : String,i: Int){
-        val database = Firebase.database
-        val level = database.getReference("users").child(auth.currentUser?.uid.toString()).child("${type}Level")
-        level.get().addOnSuccessListener{
-
-            val value1 = it.value
-            val quiz = database.getReference("${type}Quiz").child("lv$value1")
-
-            quiz.get().addOnSuccessListener {
-                val value2 : ArrayList<Any> = it.value as ArrayList<Any>
-                val valueMap: HashMap<String,String> = value2[i] as HashMap<String, String>
-
-                val word:TextView = findViewById(R.id.word)
-                val tvOptionOne:Button = findViewById(R.id.tv_option_one)
-                val tvOptionTwo:Button = findViewById(R.id.tv_option_two)
-                val tvOptionThree:Button = findViewById(R.id.tv_option_three)
-                val tvOptionFour:Button = findViewById(R.id.tv_option_four)
-
-                word.text = valueMap["word"]
-                tvOptionOne.text = valueMap["option1"]
-                tvOptionTwo.text = valueMap["option2"]
-                tvOptionThree.text = valueMap["option3"]
-                tvOptionFour.text = valueMap["option4"]
-
-
-            }
-
-        }
-
+        tvOptionFour.setBackgroundResource(R.drawable.radius_white)
+        tvOptionFour.setTextColor(Color.parseColor("#7A8089"))
     }
 
 }
