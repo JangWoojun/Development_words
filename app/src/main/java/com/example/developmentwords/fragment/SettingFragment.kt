e737538591afc2c28799ee9e23f934191f031480
JangWoojun <woojun0107@naver.com>
2022-11-27 19:02:10 +0900

    FEAT : 이메일 보내기 추가
    문의사항,오류제보 클릭 시 이메일 보내주는 기능 추가

--- app/src/main/java/com/example/developmentwords/fragment/SettingFragment.kt
+++ app/src/main/java/com/example/developmentwords/fragment/SettingFragment.kt
@@ -1,10 +1,12 @@
 package com.example.developmentwords.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.developmentwords.R
 import com.example.developmentwords.databinding.FragmentSettingBinding
@@ -14,10 +16,7 @@ class SettingFragment : Fragment() {
 
     private var _binding: FragmentSettingBinding? = null
     private val binding get() = _binding!!
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val addresses : Array<String> = arrayOf("woojun0107@naver.com")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,6 +33,30 @@ class SettingFragment : Fragment() {
         binding.home.setOnClickListener{
             it.findNavController().navigate(R.id.action_settingFragment_to_mainFragment)
         }
+
+        binding.click1.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "*/*"
+            intent.setPackage("com.google.android.gm")
+
+            intent.putExtra(Intent.EXTRA_EMAIL, addresses)
+            intent.putExtra(Intent.EXTRA_SUBJECT, "코딩보카 관련 문의")
+            intent.putExtra(Intent.EXTRA_TEXT, "(앱에 대한 문의사항 또는 건의할 점을 알려주세요!)")
+
+            startActivity(intent)
+        }
+
+        binding.click2.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "*/*"
+            intent.setPackage("com.google.android.gm")
+            intent.putExtra(Intent.EXTRA_EMAIL, addresses)
+            intent.putExtra(Intent.EXTRA_SUBJECT, "코딩보카 오류 제보")
+            intent.putExtra(Intent.EXTRA_TEXT, "(앱을 사용하시며 겪은 오류사항을 제보해주세요!)")
+
+            startActivity(intent)
+        }
+
 
 
         return binding.root
